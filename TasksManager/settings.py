65a4f2abe0c50536e750cae5a1c85e7d7fb4e495
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-11-29 23:24:51 +0100

    feat: add apps to settings

--- TasksManager/settings.py
+++ TasksManager/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_filters',
+
+    'core',
+    'accounts',
+    'boards',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,7 +63,7 @@ ROOT_URLCONF = 'TasksManager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
