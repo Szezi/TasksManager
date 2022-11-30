d16d2e41832520fe6154cb88b2776c9344192514
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-11-30 22:41:42 +0100

    feat: create Custom Login and Register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 
-# Create your views here.
+from .forms import MyUserCreationForm
+from django.contrib.auth import get_user_model, login
+
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/sign-in.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('dashboard')
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('dashboard')
+        return super(CustomLoginView, self).get(*args, **kwargs)
+
+
+class CustomRegisterView(FormView):
+    template_name = 'accounts/sign-up.html'
+    form_class = MyUserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(CustomRegisterView, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('dashboard')
+        return super(CustomRegisterView, self).get(*args, **kwargs)
