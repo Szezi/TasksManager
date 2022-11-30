a7bcd7e1cf0c9c6e5a4994bbc0518d07403f5a2b
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-11-30 22:42:18 +0100

    feat: add urls to log in, register and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from .views import CustomLoginView, CustomRegisterView
+from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-
+    path('login', CustomLoginView.as_view(), name='login'),
+    path('register', CustomRegisterView.as_view(), name='register'),
+    path('logout', LogoutView.as_view(next_page='home'), name='logout'),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
