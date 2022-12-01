5c7f4971c4160e69813d3d8acf355f819306c169
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-12-01 23:28:09 +0100

    feat: add profile info page to urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CustomLoginView, CustomRegisterView, UserDetail, UserUpdate
+from .views import CustomLoginView, CustomRegisterView, UserDetail, UserUpdate, UserInfo
 from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login', CustomLoginView.as_view(), name='login'),
     path('register', CustomRegisterView.as_view(), name='register'),
     path('logout', LogoutView.as_view(next_page='home'), name='logout'),
+    path('profile-info/<int:pk>/', UserInfo.as_view(), name='profile-detail'),
     path('profile-detail', UserDetail.as_view(), name='profile-detail'),
     path('profile-update', UserUpdate.as_view(), name='profile-update'),
 ]
