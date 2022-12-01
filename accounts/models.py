909d4f58ed1b05c92c01cb0452e5ebc9d3f70be3
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-12-01 22:13:42 +0100

    feat: add additional fields to User model

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,10 @@ class User(AbstractUser):
         max_length=255, blank=False, null=False, unique=True)
     first_name = models.CharField(max_length=255, blank=False, null=True)
     last_name = models.CharField(max_length=255, blank=False, null=True)
+    description = models.CharField(max_length=100, default='Users desc.')
+    city = models.CharField(max_length=100, default='Unknown city')
+    phoneNumber = models.IntegerField(default=555666333)
+    bio = models.CharField(max_length=225, default='Users bio')
     avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'username'
