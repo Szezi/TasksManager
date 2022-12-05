8f240a045a6411a52aa0106985ec74a3d12dff57
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-12-05 18:34:47 +0100

    feat: add urls to password/email change pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import CustomLoginView, CustomRegisterView, UserDetail, UserUpdate, UserInfo
-from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
+from .views import CustomLoginView, CustomRegisterView, UserDetail, UserUpdate, UserInfo, EmailChangeView, CustomPasswordChangeView
 
 urlpatterns = [
     path('login', CustomLoginView.as_view(), name='login'),
@@ -11,6 +11,8 @@ urlpatterns = [
     path('profile-info/<int:pk>/', UserInfo.as_view(), name='profile-detail'),
     path('profile-detail', UserDetail.as_view(), name='profile-detail'),
     path('profile-update', UserUpdate.as_view(), name='profile-update'),
+    path('password-change', CustomPasswordChangeView.as_view(), name='password-change'),
+    path('email-change', EmailChangeView.as_view(), name='email-change'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
