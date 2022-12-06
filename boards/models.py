f878c4d123c4b8f2f3c1db2e40f34e3e72e9979e
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-12-06 22:00:35 +0100

    feat: create Task and TasksBoard model

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,48 @@
+from django.conf import settings
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class TasksBoard(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(default='New Board', max_length=200)
+    description = models.TextField(null=True, blank=True, default='Board description')
+    members = models.ManyToManyField(
+        User, related_name='members', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    board = models.ForeignKey(TasksBoard, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(default='New task', max_length=200)
+    description = models.TextField(null=True, blank=True, default='Task description')
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    assigned_to = models.ManyToManyField(
+        User, related_name='assigned_to', blank=True)
+
+    class State(models.IntegerChoices):
+        next = 1, "NEXT"
+        in_progress = 2, "IN PROGRESS"
+        done = 3, "DONE"
+
+    state = models.PositiveSmallIntegerField(
+        choices=State.choices,
+        default=State.next
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['deadline']
+
+
+
+
