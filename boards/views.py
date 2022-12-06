e0e33ecaf0a1c5d22c37fef9ef4557e1af84fafc
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-12-06 22:02:10 +0100

    feat: create Task/TasksBoard CRUD views

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,104 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.views.generic.list import ListView
+
+from .models import Task, TasksBoard
 
 
 class DashboardView(TemplateView):
     template_name = "boards/dashboard.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('home')
+        return super(DashboardView, self).dispatch(request, *args, **kwargs)
+
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    template_name = 'boards/task_form.html'
+    model = Task
+    fields = ['title', 'description', 'deadline', 'state', 'board', 'assigned_to']
+    success_url = reverse_lazy('dashboard')
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('home')
+        return super(TaskCreate, self).dispatch(request, *args, **kwargs)
+
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    template_name = 'boards/task_update.html'
+    model = Task
+    fields = ['title', 'description', 'deadline', 'state', 'board', 'assigned_to']
+    context_object_name = 'task'
+    success_url = reverse_lazy('dashboard')
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('home')
+        return super(TaskUpdate, self).dispatch(request, *args, **kwargs)
+
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('dashboard')
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('home')
+        return super(TaskDelete, self).dispatch(request, *args, **kwargs)
+
+
+class TasksBoardDetail(LoginRequiredMixin, DetailView):
+    template_name = 'boards/board_detail.html'
+    model = TasksBoard
+    context_object_name = 'board'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('home')
+        return super(TasksBoardDetail, self).dispatch(request, *args, **kwargs)
+
+
+class TasksBoardCreate(LoginRequiredMixin, CreateView):
+    template_name = 'boards/board_form.html'
+    model = TasksBoard
+    fields = ['name', 'description', 'members']
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TasksBoardCreate, self).form_valid(form)
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('home')
+        return super(TasksBoardCreate, self).dispatch(request, *args, **kwargs)
+
+
+class TasksBoardUpdate(LoginRequiredMixin, UpdateView):
+    template_name = 'boards/board_update.html'
+    model = TasksBoard
+    fields = ['name', 'description', 'members']
+    context_object_name = 'board'
+    success_url = reverse_lazy('dashboard')
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('home')
+        return super(TasksBoardUpdate, self).dispatch(request, *args, **kwargs)
+
+
+class TasksBoardDelete(LoginRequiredMixin, DeleteView):
+    model = TasksBoard
+    context_object_name = 'board'
+    success_url = reverse_lazy('dashboard')
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('home')
+        return super(TasksBoardDelete, self).dispatch(request, *args, **kwargs)
