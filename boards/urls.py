d94bba4fed00eec45a6121b5fa2ff3db6c81a3c4
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-12-06 22:02:27 +0100

    feat: add Task/TasksBoard CRUD urls

--- boards/urls.py
+++ boards/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import DashboardView
+from .views import DashboardView, TaskCreate, TaskUpdate, TaskDelete, TasksBoardDetail, TasksBoardCreate, TasksBoardUpdate, TasksBoardDelete
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard', DashboardView.as_view(), name='dashboard'),
+
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+
+    path('board-create/', TasksBoardCreate.as_view(), name='board-create'),
+    path('board-detail/<int:pk>/', TasksBoardDetail.as_view(), name='board-detail'),
+    path('board-update/<int:pk>/', TasksBoardUpdate.as_view(), name='board-update'),
+    path('board-delete/<int:pk>/', TasksBoardDelete.as_view(), name='board-delete')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
