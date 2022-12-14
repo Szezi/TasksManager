45f8de5e07143dd00432736ae6681411ab320409
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-12-14 19:54:43 +0100

    feat: redirect if user logged in

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = "core/home.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        return super(HomeView, self).dispatch(request, *args, **kwargs)
