6faa83ee379764a7ce574b715006e51255faa6ff
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-12-14 19:57:49 +0100

    fix: change boards fields owner to admin with m2m rel.

--- boards/models.py
+++ boards/models.py
@@ -4,9 +4,9 @@ from accounts.models import User
 
 
 class TasksBoard(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    admin = models.ManyToManyField(User, related_name='admin', null=False)
     name = models.CharField(default='New Board', max_length=200)
-    description = models.TextField(null=True, blank=True, default='Board description')
+    description = models.CharField(max_length=250, null=True, blank=True, default='Board description')
     members = models.ManyToManyField(
         User, related_name='members', blank=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -19,7 +19,7 @@ class TasksBoard(models.Model):
 
 
 class Task(models.Model):
-    board = models.ForeignKey(TasksBoard, on_delete=models.CASCADE, null=False, blank=True)
+    board = models.ForeignKey(TasksBoard, on_delete=models.CASCADE, null=False)
     title = models.CharField(default='New task', max_length=200)
     description = models.CharField(max_length=100, null=True, blank=True, default='Task description')
     notes = models.TextField(null=True, blank=True)
@@ -50,6 +50,6 @@ class PinedTask(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return "PinedTask"
+        return self.owner.username
 
 
