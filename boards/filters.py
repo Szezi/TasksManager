5a0b157a10a60f48a9cdfdfef6fdb13ecf994c46
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-12-23 19:42:21 +0100

    fix: hide notes fireld

--- boards/filters.py
+++ boards/filters.py
@@ -7,9 +7,8 @@ from .models import *
 class TaskFilter(django_filters.FilterSet):
     title = CharFilter(field_name='title', lookup_expr='icontains')
     description = CharFilter(field_name='description', lookup_expr='icontains')
-    notes = CharFilter(field_name='notes', lookup_expr='icontains')
 
     class Meta:
         model = Task
         fields = '__all__'
-        exclude = ['board', 'created', 'state', 'deadline']
+        exclude = ['board', 'created', 'state', 'deadline', 'notes']
